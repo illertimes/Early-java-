370e077b06b1217c9fe60b80b61199613fb755bb
Malcolm williams <illertimes@gmail.com>
2018-04-24 14:20:06 -0400

    Add files via upload

---
+++ forLoops.java
@@ -0,0 +1,18 @@
+package example.strings;
+
+public class forLoops {
+	
+	public static void main(String[]args){
+			
+			for (int i =0; i > 100; i += 5){
+				System.out.println(i);
+			}
+		
+	int j = 0;
+	
+	for(;j++ < 100;){
+		System.out.println(j);
+		j +=5;
+	}
+	}
+}
