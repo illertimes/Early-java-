3d84543f38aa6c3e164449530a1455bafb7279c5
Malcolm williams <illertimes@gmail.com>
2018-04-25 13:58:09 -0400

    Add files via upload

---
+++ Assignment5.java
@@ -0,0 +1,34 @@
+package assignments;
+
+public class Assignment5 {
+	 public static void main(String[] args) {        
+	        for (int i=10; i<=20; i+=2)
+	        {
+	            if (i==16) 
+	                continue;
+	            System.out.println(i);
+	        }
+	          
+	        for (int i=10;i<=20;i++)
+	        {
+	            if (i%2 == 1) 
+	                continue;               
+	            if (i==16) 
+	                continue;
+	             
+	            System.out.println(i);
+	        }
+	          
+	          
+	        for (int i=10;;i+=2)
+	        {
+	            if (i==16) 
+	                continue;
+	            if (i>20) 
+	                break;
+	             
+	            System.out.println(i);
+	              
+	        }       
+	    }
+}
